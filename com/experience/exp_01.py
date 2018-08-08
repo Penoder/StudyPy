e89b861db3f42498f75cf259958a70a1ae859a3e
Penoder <Rocking7189@163.com>
2018-08-09 00:55:46 +0800

    2018/08/09

--- com/experience/exp_01.py
+++ com/experience/exp_01.py
@@ -1,3 +1,14 @@
-number = input("请输入需要猜测的数字：")
-num = int(number)
-print(num)
+"""
+    练习1： 有 1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？ 都是多少
+"""
+count = 0
+for i in range(1, 5):
+    for j in range(1, 5):
+        for k in range(1, 5):
+            if (i != j and i != k and j != k):
+                count += 1
+                print(i, j, k)
+
+print(count)
+
+# 总共有 24 个数
